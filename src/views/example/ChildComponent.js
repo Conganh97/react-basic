3a3114b5a7e7aec5c657870d7ceb15ede8736453
Bùi Công Anh <anhbc@etc.vn>
2023-12-07 11:06:05 +0700

    up #18

--- src/views/example/ChildComponent.js
+++ src/views/example/ChildComponent.js
@@ -2,12 +2,69 @@ import React from "react";
 
 class ChildComponent extends React.Component {
   state = {
-    firstName: "",
-    lastName: "",
+    showJobs: false,
   };
 
+  handleShowHide = () => {
+    this.setState({
+      showJobs: !this.state.showJobs,
+    });
+  };
+
+  handleDelete = (job) => {
+    this.props.deleteJob(job);
+  };
   render() {
-    return <div>Child Component: {this.props.name}</div>;
+    const { jobs } = this.props;
+    const { showJobs } = this.state;
+
+    return (
+      <>
+        {!showJobs ? (
+          <div>
+            <button onClick={() => this.handleShowHide()}>Show</button>
+          </div>
+        ) : (
+          <>
+            <div className="jobs">
+              {jobs.map((item, index) => {
+                if (item.salary > 1000) {
+                  return (
+                    <div key={item.id}>
+                      {item.title} - {item.salary}$ -
+                      <span onClick={() => this.handleDelete(item)}> x</span>
+                    </div>
+                  );
+                }
+              })}
+            </div>
+            <div>
+              <button onClick={() => this.handleShowHide()}>Hide</button>
+            </div>
+          </>
+        )}
+      </>
+    );
   }
 }
+// const ChildComponent = (props) => {
+//   console.log(">>>> props", props);
+//   const { jobs } = props;
+
+//   return (
+//     <>
+//       <div className="jobs">
+//         {jobs.map((item, index) => {
+//           if (item.salary > 1000) {
+//             return (
+//               <div key={item.id}>
+//                 {item.title} - {item.salary}$
+//               </div>
+//             );
+//           }
+//         })}
+//       </div>
+//     </>
+//   );
+// };
 export default ChildComponent;
--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -1,55 +1,36 @@
 import React from "react";
 import ChildComponent from "./ChildComponent";
+import AddComponent from "./AddComponent";
 
 class MyComponent extends React.Component {
   state = {
-    firstName: "",
-    lastName: "",
+    arrJobs: [
+      { id: "abc1", title: "Cong Anh 1", salary: 1000 },
+      { id: "abc2", title: "Cong Anh 2", salary: 1500 },
+      { id: "abc3", title: "Cong Anh 3", salary: 2000 },
+    ],
   };
 
-  handleChangeFirstName = (event) => {
+  addNewJob = (job) => {
     this.setState({
-      firstName: event.target.value,
+      arrJobs: [...this.state.arrJobs, job],
     });
   };
 
-  handleChangeLastName = (event) => {
+  deleteJob = (job) => {
+    let currentJob = this.state.arrJobs;
+    currentJob = currentJob.filter((item) => item.id !== job.id);
     this.setState({
-      lastName: event.target.value,
+      arrJobs: currentJob,
     });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    alert("Click submit");
   };
 
   render() {
     return (
       <>
-        {console.log(">>> render", this.state)}
-        <form>
-          <label htmlFor="fname">First name:</label>
-          <br />
-          <input
-            type="text"
-            value={this.state.firstName}
-            onChange={(event) => this.handleChangeFirstName(event)}
-          />
-          <br />
-          <label htmlFor="lname">Last name:</label>
-          <br />
-          <input
-            type="text"
-            value={this.state.lastName}
-            onChange={(event) => this.handleChangeLastName(event)}
-          />
-          <br />
-          <input type="submit" onClick={(event) => this.handleSubmit(event)} />
-        </form>
-        <ChildComponent name={"child one"} />
-        <ChildComponent name={"child two"} />
-        <ChildComponent name={"child three"} />
+        <AddComponent addNewJob={this.addNewJob} />
+
+        <ChildComponent jobs={this.state.arrJobs} deleteJob={this.deleteJob} />
       </>
     );
   }
